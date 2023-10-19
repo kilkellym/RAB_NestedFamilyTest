a18a509af23e8c98ef48f2c0a722b6204585ec02
kilkellym <michael@archsmarter.com>
2023-10-19 16:44:11 -0400

    WIP

--- RAB_NestedFamilyTest/Command2.cs
+++ RAB_NestedFamilyTest/Command2.cs
@@ -7,6 +7,7 @@ using Autodesk.Revit.UI.Selection;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.Linq;
 using System.Reflection;
 
 #endregion
@@ -16,6 +17,7 @@ namespace RAB_NestedFamilyTest
     [Transaction(TransactionMode.Manual)]
     public class Command2 : IExternalCommand
     {
+        public int counter;
         public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
         {
             // this is a variable for the Revit application
@@ -24,10 +26,73 @@ namespace RAB_NestedFamilyTest
             // this is a variable for the current Revit model
             Document doc = uiapp.ActiveUIDocument.Document;
 
-            // Your code goes here
+            FilteredElementCollector collector = new FilteredElementCollector(doc, doc.ActiveView.Id);
+            collector.OfClass(typeof(FamilyInstance));
 
+            List<Element> listFamilyInstances = new FilteredElementCollector(doc, doc.ActiveView.Id)
+            .OfClass(typeof(FamilyInstance))
+            .Cast<FamilyInstance>()
+            .Where(a => a.SuperComponent == null)
+            .SelectMany(a => a.GetSubComponentIds())
+            .Select(a => doc.GetElement(a))
+            .ToList();
+
+            counter = 0;
+
+            foreach (FamilyInstance curFI in collector)
+            {
+                Family curFam = curFI.Symbol.Family;
+                GetNestedFamilies(doc, curFam, 0);
+            }
+            
+            // output results to OUTPUT window
+            TaskDialog.Show("Complete", $"Set {counter} families to shared.");
 
             return Result.Succeeded;
+        }
+        private void GetNestedFamilies(Document doc, Family curFamily, int count)
+        {
+            counter++;
+
+            // to prevent stack overflow error
+            if (count > 100)
+                return;
+
+            using(Transaction t = new Transaction(doc))
+            {
+                t.Start("Set shared param");
+                // set as shared
+                curFamily.get_Parameter(BuiltInParameter.FAMILY_SHARED).Set(1);
+                t.Commit();
+            }
+            
+
+            // Get Family document for family
+            Document familyDoc = doc.EditFamily(curFamily);
+            
+
+            familyDoc.OwnerFamily.get_Parameter(BuiltInParameter.FAMILY_SHARED).Set(1);
+
+            if (null != familyDoc && familyDoc.IsFamilyDocument == true)
+            {
+                try
+                {
+                    FilteredElementCollector collector1 = new FilteredElementCollector(familyDoc);
+                    collector1.OfClass(typeof(Family)).ToElements();
+
+                    if (collector1.Count() > 0)
+                    {
+                        foreach (Family curFI2 in collector1)
+                        {
+                            GetNestedFamilies(familyDoc, curFI2, count);
+                        }
+                    }
+                }
+                catch (Exception e)
+                {
+                    Debug.Print(e.Message);
+                }
+            }
         }
         internal static PushButtonData GetButtonData()
         {
